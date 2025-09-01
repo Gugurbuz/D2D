4b748bbcb4dcc912ac7d88a95181c1b0217ca5a3
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-09-01 23:37:26 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ThemeProvider } from './context/ThemeContext'; // YENİ: Import et
+import { ThemeProvider } from './context/ThemeContext.tsx'; // YENİ: Import et
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
