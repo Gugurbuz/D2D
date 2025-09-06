9eb786bedb859021b6dad05ad7d59912a7e2537b
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-09-07 00:55:47 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -113,95 +113,55 @@ function App() {
   }
 
   const renderScreen = () => {
-    switch (currentScreen) {
-      case 'dashboard':
-        return (
-          <DashboardScreen 
-            customers={customers}
-            assignments={assignments}
-            allReps={allReps}
-            setCurrentScreen={setCurrentScreen}
-            onSelectCustomer={handleSelectCustomer}
-          />
-        );
-      case 'route':
-        return (
-          <RouteMapScreen 
-            customers={customers}
-            salesRep={{ name: agentName, lat: 40.9368, lng: 29.1553 }}
-          />
-        );
-      case 'visits':
-        return (
-          <VisitListScreen
-            customers={customers}
-            assignments={assignments}
-            allReps={allReps}
-            setCurrentScreen={setCurrentScreen}
-            onSelectCustomer={handleSelectCustomer}
-          />
-        );
-      case 'visitDetail':
-        return selectedCustomer ? (
-          <VisitDetailScreen
-            customer={selectedCustomer}
-            onBack={() => setCurrentScreen('visits')}
-            onStartVisit={(c) => {
-              setSelectedCustomer(c);
-              setCurrentScreen('visitFlow');
-            }}
-          />
-        ) : null;
-      case 'visitFlow':
-        return selectedCustomer ? (
-          <VisitFlowScreen
-            customer={selectedCustomer}
-            onCloseToList={() => setCurrentScreen('visits')}
-            onCompleteVisit={() => setCurrentScreen('visits')}
-          />
-        ) : null;
-      case 'reports':
-        return <ReportsScreen customers={customers} />;
-      case 'assignments':
-        return (
-          <AssignmentScreen
-            customers={customers}
-            assignments={assignments}
-            setAssignments={setAssignments}
-            allReps={allReps}
-            setCurrentScreen={setCurrentScreen}
-          />
-        );
-      case 'assignmentMap':
-        return (
-          <AssignmentMapScreen
-            customers={customers}
-            assignments={assignments}
-            setAssignments={setAssignments}
-            allReps={teamReps}
-            onBack={() => setCurrentScreen('assignments')}
-          />
-        );
-      case 'team':
-        return <TeamMapScreen reps={teamReps} />;
-      case 'messages':
-        return <MessagesScreen />;
-      case 'profile':
-        return <ProfileScreens role="sales" />;
-      case 'invoiceOcr':
-        return <InvoiceOcrPage />;
-      default:
-        return (
-          <DashboardScreen 
-            customers={customers}
-            assignments={assignments}
-            allReps={allReps}
-            setCurrentScreen={setCurrentScreen}
-            onSelectCustomer={handleSelectCustomer}
-          />
-        );
-    }
-  };
+  switch (currentScreen) {
+    case 'dashboard':
+      return (
+        <DashboardScreen 
+          customers={customers}
+          assignments={assignments}
+          allReps={allReps}
+          setCurrentScreen={setCurrentScreen}
+          onSelectCustomer={handleSelectCustomer}
+        />
+      );
+    case 'route':
+      return (
+        <RouteMapScreen 
+          customers={customers}
+          salesRep={{ name: agentName, lat: 40.9368, lng: 29.1553 }}
+        />
+      );
+    case 'visits':
+      return (
+        <VisitListScreen
+          customers={customers}
+          assignments={assignments}
+          allReps={allReps}
+          setCurrentScreen={setCurrentScreen}
+          onSelectCustomer={handleSelectCustomer}
+        />
+      );
+    case 'userManagement':
+      return <UserManagementScreen />;
+    case 'systemSettings':
+      return <SystemSettingsScreen />;
+    case 'tariffs':
+      return <TariffsScreen />;
+    case 'fieldOpsMap':
+      return <FieldOpsMapScreen />;
+    default:
+      return (
+        <DashboardScreen 
+          customers={customers}
+          assignments={assignments}
+          allReps={allReps}
+          setCurrentScreen={setCurrentScreen}
+          onSelectCustomer={handleSelectCustomer}
+        />
+      );
+  }
+};
+
 
   return (
     <AppLayout
