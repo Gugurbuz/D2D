0c233eea17919096f23295026d5743fe74c88231
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-09-06 18:04:35 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,14 @@ export default {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'), // (önerilir) form elemanlarını normalize eder
-  ],
-};
-safelist: [
+  safelist: [
   'bg-green-100', 'text-green-800',
   'bg-blue-100', 'text-blue-800',
   'bg-yellow-100', 'text-yellow-800',
   'bg-red-100', 'text-red-800'
 ]
+  plugins: [
+    require('@tailwindcss/forms'), // (önerilir) form elemanlarını normalize eder
+  ],
+};
+
