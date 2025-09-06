9d69c8879fde09a5bbd47b9c3e3c8438f352afe6
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-09-07 00:51:12 +0300

    Updated SystemSettingsScreen.tsx

---
+++ src/screens/SystemSettingsScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SystemSettingsScreen: React.FC = () => {
+  return (
+    <div className="p-6 bg-white rounded-xl shadow">
+      <h1 className="text-xl font-bold mb-2">Sistem Ayarları</h1>
+      <p className="text-gray-600">Sistem ayarları burada yapılandırılacak.</p>
+    </div>
+  );
+};
+
+export default SystemSettingsScreen;
