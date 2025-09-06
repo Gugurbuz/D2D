5241952fc03a94f3a8bd424907cd5bafb1afe06d
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-09-06 18:04:17 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,3 +16,9 @@ export default {
     require('@tailwindcss/forms'), // (önerilir) form elemanlarını normalize eder
   ],
 };
+safelist: [
+  'bg-green-100', 'text-green-800',
+  'bg-blue-100', 'text-blue-800',
+  'bg-yellow-100', 'text-yellow-800',
+  'bg-red-100', 'text-red-800'
+]
