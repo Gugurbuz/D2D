cba9e4d44e5540e591e6816bf239bb870013c7fa
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-09-07 02:23:52 +0300

    Updated DashboardScreen.tsx

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -112,7 +112,7 @@ const DashboardScreen: React.FC<Props> = ({ customers, assignments, allReps, set
       {announcements.join("   •   ")}
     </div>
   </div>
-</div>
+
 
       {/* KPI Kartları */}
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
