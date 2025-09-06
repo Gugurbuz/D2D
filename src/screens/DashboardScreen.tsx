683984c1c1a1dee685b2e8ff17d4ae2c572ec260
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-09-07 02:27:44 +0300

    Updated DashboardScreen.tsx

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -112,7 +112,7 @@ const DashboardScreen: React.FC<Props> = ({ customers, assignments, allReps, set
       {announcements.join("   •   ")}
     </div>
   </div>
-</div>
+
 
 
       {/* KPI Kartları */}
