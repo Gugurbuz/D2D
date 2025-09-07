c9efd23e73a7e8012665b637da618475775a3ad2
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-09-07 03:18:46 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,3 +24,21 @@ export default {
 ]
 };
 
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        marquee: 'marquee 15s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
