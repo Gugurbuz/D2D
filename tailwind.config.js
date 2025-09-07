939195e670c773864989fd707c37940f4f1013c9
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-09-07 03:44:38 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 // tailwind.config.js
 module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 senin src yollarını kapsamalı
+  ],
   theme: {
     extend: {
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
       animation: {
-        marquee: 'marquee 15s linear infinite',
+        marquee: "marquee 15s linear infinite",
       },
     },
   },
