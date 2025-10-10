ff0a14d70947c00111cf640214e1fd560803d60d
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-10-10 21:05:28 +0300

    Updated taskFlowStore.ts

--- src/store/taskFlowStore.ts
+++ src/store/taskFlowStore.ts
@@ -1,4 +1,4 @@
-import { create } from 'zustand';
+import { create } from 'zustand/react';
 import type { Database } from '../lib/database.types';
 
 type VisitStatus = Database['public']['Enums']['visit_status'];
